72ab8b74853e3ea21bc14b04f1a82069cb020d54
Aryan 'Ba3a' Kumar <aryan@gamzo.in>
2022-07-28 14:06:56 +0530

    Cleaning up

--- Recursion/decimalToBinary.py
+++ Recursion/decimalToBinary.py
@@ -4,7 +4,6 @@ def decimalToBinary(number, binary=""):
     return decimalToBinary(number//2, str(number%2)+binary)
 
 #Driver code
-"""if __name__ == "__main__":
-    print(f"I convert decimal to binary, for example 13 is {decimalToBinary(13)}.")"""
+if __name__ == "__main__":
+    print(f"I convert decimal to binary, for example 13 is {decimalToBinary(13)}.")
 
-print(decimalToBinary(39))
