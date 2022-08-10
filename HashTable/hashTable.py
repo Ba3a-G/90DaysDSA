b1e5cd5a7dc309030fc42a039593af04c211c1a8
Aryan Kumar <aryan.ak434@gmail.com>
2022-08-11 04:09:33 +0530

    added collison handling

--- HashTable/hashTable.py
+++ HashTable/hashTable.py
@@ -1,9 +1,11 @@
 class HashTable():
     def __init__(self):
-        self.arr = [None] * 10
+        self.arr = [[] for i in range(10)]
 
     def printAll(self):
-        print(self.arr)
+        for i in range(len(self.arr)):
+            for element in self.arr[i]:
+                print(f"{element[0]}: {element[1]}")
 
     def getHash(self, string):
         h=0
@@ -13,16 +15,32 @@ class HashTable():
 
     def __setitem__(self, key, value):
         h = self.getHash(key)
-        self.arr[h] = value
+        existing = False
+        for idx, val in enumerate(self.arr[h]):
+            if val[0] == key:
+                self.arr[h][idx] = (key, value)
+                existing = True
+        if not existing:
+            self.arr[h].append((key, value))
 
     def __getitem__(self, key):
         h = self.getHash(key)
-        return self.arr[h]
+        for k, value in self.arr[h]:
+            if k == key:
+                return value
+
+    def __delitem__(self, key):
+        h = self.getHash(key)
+        for idx, value in enumerate(self.arr[h]):
+            if value[0] == key:
+                self.arr[h].pop(idx)
 
 if __name__ == "__main__":
     ht = HashTable()
-    ht["march 1"] = 31
-    ht["march 2"] = 32
-    ht["march 3"] = 33
+    ht["march 6"] = "6 init"
+    ht["march 7"] = "7"
+    ht["march 6"] = "6 new"
+    ht["march 17"] = "17"
     ht.printAll()
-    print(ht["march 1"])
+    del ht["march 6"]
+    print(ht["march 6"])
